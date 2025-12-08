d2d195e4ca47e19a04cb55c42de9e750d36d0547
Hardik144 <patidarhardik81@gmail.com>
2025-12-08 14:12:44 +0530

    Update welcome section and App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
-<<<<<<< HEAD
 import Navbar from '#components/Navbar'
 import Welcome from '#components/Welcome'
-=======
-import { Navbar, Welcome,  } from '#components'
->>>>>>> refs/remotes/origin/feat/welcome
 import React from 'react'
 
 const App = () => {
   return (
     <main>
       <Navbar />
-<<<<<<< HEAD
       <Welcome />
-=======
-      <Welcome /> 
->>>>>>> refs/remotes/origin/feat/welcome
     </main>
   )
 }
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -85,11 +85,7 @@ const Welcome = () => {
       </h1>
 
       <div className="small-screen">
-<<<<<<< HEAD
-        <p>This Portfolio is designed for desktop/tabletscreens only.</p>
-=======
         <p>This Portfolio is designed for desktop/tablet screens only.</p>
->>>>>>> refs/remotes/origin/feat/welcome
       </div>
     </section>
   );
